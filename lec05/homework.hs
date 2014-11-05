d9e792dd9f709848ffb6a7422f413df92641f462
yossarin <yossarin.github@gmail.com>
2014-11-05 14:47:16 +0100

    Progress HW5:3.c

--- lec05/homework.hs
+++ lec05/homework.hs
@@ -25,9 +25,81 @@ mean :: DiscreteRandVar -> Double
 mean [(x, p)]    = fromIntegral x * p
 mean ((x, p):xs) = fromIntegral x * p + mean xs
 
-meanHelp' :: DiscreteRandVar -> Double
+meanHelp' :: DiscreteRandVar -> Double -> Double
 meanHelp' [] n          = n
-meanHelp' ((x, p):xs) n = mean' xs (fromIntegral x * p + n)
+meanHelp' ((x, p):xs) n = meanHelp' xs (fromIntegral x * p + n)
 
 mean' :: DiscreteRandVar -> Double
 mean' x = meanHelp' x 0
+
+-- (b) explicitly recursive function variance and an accumulator-style recursive 
+--     function variance' that calculate the variance of a discrete random variable
+
+varianceRec :: DiscreteRandVar -> Double -> Double
+varianceRec [(x, p)] m    = (fromIntegral x - m)^2 * p
+varianceRec ((x, p):xs) m = (fromIntegral x - m)^2 * p + varianceRec xs m
+
+variance :: DiscreteRandVar -> Double
+variance xs = varianceRec xs $ mean' xs
+
+varianceAcc :: DiscreteRandVar -> Double -> Double -> Double
+varianceAcc [] n m          = n
+varianceAcc ((x, p):xs) n m = varianceAcc xs ((fromIntegral x - m)^2 * p + n) m
+
+variance' :: DiscreteRandVar -> Double
+variance' xs = varianceAcc xs 0 $ mean' xs
+
+-- (c) explicitly recursive function probabilityFilter and an accumulator style 
+--     recursive function probabilityFilter' that take a probability and a 
+--     random variable and return a list of values that have at least the given 
+--     probability of appearing, in the same order in which they appear in the 
+--     random variable definition
+
+probabilityFilter :: Probability -> DiscreteRandVar -> [Int]
+probabilityFilter prob ((x, p):[]) | p >= prob = [x]
+                                   | otherwise = []
+								
+probabilityFilter prob ((x, p):xs) | p >= prob = x : probabilityFilter prob xs
+                                   | otherwise = probabilityFilter prob xs
+
+probAcc :: Probability -> DiscreteRandVar -> [Int] -> [Int]
+probAcc _ [] acc = acc
+probAcc prob ((x, p):xs) acc | p >= prob = probAcc prob xs (acc ++ [x])
+                             | otherwise = probAcc prob xs acc
+								   
+probabilityFilter' :: Probability -> DiscreteRandVar -> [Int]
+probabilityFilter' prob xs = probAcc prob xs []
+
+{-
+  3.) Often we need to split up a list into smaller chunks.
+-}
+
+-- (a) function chunk splits up a list xs into sublist of length n. If the
+--     length of xs is not a multiple of n, the last sublist will be shorter than n. 
+
+chunk :: Int -> [a] -> [[a]]
+chunk 0 _ = []
+chunk _ [] = []
+chunk n xs = take n xs : chunk n ( drop n xs )
+
+-- (b) function chunkBy splits up a list xs into sublists of lengths given
+--     in a list of indices is. If the lengths in is do not add up to the length
+--     of xs, the remaining part of xs will remain unchunked
+
+chunkBy :: [Int] -> [a] -> [[a]]
+chunkBy [] _ = []
+chunkBy _ [] = []
+chunkBy (y:ys) xs = take y xs : chunkBy ys (drop y xs)
+
+-- (c) function chunkInto splits up a list xs into n sublists of equal
+--     length. If the length of xs is not divisible by n, chunk the 
+--     remainder into the last sublist
+
+chunkInto :: Int -> [a] -> [[a]]
+chunkInto 0 _  = []
+chunkInto _ [] = []
+chunkInto n xs
+  | (length xs) < 2*t = [xs] 
+  | otherwise         = take t xs : chunkInto (n-1) (drop t xs)
+    where t = (truncate $ fromIntegral (length xs) / fromIntegral n)
+
