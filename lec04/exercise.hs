4eb4ad6a1677870cf1ed76d6baf5b92f5cb858ae
unknown <yossarin.github@gmail.com>
2014-11-10 20:56:07 +0100

    Added 6. homework.

--- lec04/exercise.hs
+++ lec04/exercise.hs
@@ -1,30 +1,19 @@
 import Data.Char
 import Data.List
 
-fst' :: (a,b) -> a
-fst' (x,_) = x
-
-addVectors :: (Double, Double) -> (Double, Double) ->  (Double, Double)
-addVectors (x1,y1) (x2,y2) = (x1 + x2, y1 + y2)
-
-swap :: (a,b) -> (b,a)
-swap (x,y) = (y,x)
-
-mITM :: (a,b,c) -> b
-mITM (_,y,_) = y
-
-leaves :: ((a, a), (a, a)) -> [a]
-leaves ((x,y), (z,w)) = [x,y,z,w]
-
-head' :: [a] -> a
-head' [] = error "No head to behead"
-head' (x:_) = x
-
 headHunter :: [[a]] -> a
 headHunter ((x:xs):xss) = x
 headHunter (_:(x:xs):xss) = x
 headHunter (_:_:(x:xs):_) = x
 headHunter _ = error "Error"
+
+isWellFormed l | l == [[]] = False
+               | otherwise = length (nub [length e | e <- l ])== 1
+
+firstColumn m = [e!!0 | e <- m]
+
+shoutOutLoud :: String -> String
+shoutOutLoud s = unwords [ w!!0:w!!0:w | w <- words s]
 
 pad (s1h:s1t) (s2h:s2t) = (toUpper s1h: (fill s1t), toUpper s2h : (fill s2t))
   where l = max (length s1t) (length s2t)
@@ -39,15 +28,4 @@ pad' (x:xs) (y:ys) =
  
   in  ((toUpper x : (fill xs)), (toUpper y : (fill ys)))
 
-magicNumber :: Int -> String
-magicNumber x = case x of
-  42 -> "Yeah!"
-  _  -> "Nope, try again."
-  
-takePair (x,y) (_:e:_) = "The pair "++(case (x,y) of
-  (1,1) -> "contains two ones "
-  (1,_) -> "contains one one "
-  (_,1) -> "contains one one "
-  _     -> "does not contain a single one ")++
-  "and the second elem of the list is " ++ show e
 
