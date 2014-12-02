743da3b50bf11727362e7eee89f0a0eef8f73e39
unknown <yossarin.github@gmail.com>
2014-12-02 18:14:45 +0100

    Start of HW 7

--- lec07/exercise.hs
+++ lec07/exercise.hs
@@ -10,6 +10,9 @@ sumEven = sum . map snd . filter (even . fst) . zip [0..]
 filterWords :: [String] -> String -> String
 filterWords ws = unwords . filter (`notElem` ws) . words
 
+initials3 :: String -> (String -> Bool) -> String -> String
+initials3 d p = concat . map (\x -> (toUpper . head $ x):".") . filter (p) . words 
+
 -- EXERCISE 2
 
 maxDiff :: [Int] -> Int
@@ -20,15 +23,28 @@ diffUni xs f = f . map (abs . uncurry (-)) . zip xs $ tail xs
 maxMinDiff :: [Int] -> (Int, Int)
 maxMinDiff xs = (diffUni xs minimum, diffUni xs maximum)
 
+
 -- EXERCISE 3
 
 isTitleCased :: String -> Bool
 isTitleCased = all (isUpper . head) .  words
 
+sortPairs :: Ord b => [(a,b)] -> [(a,b)]
+sortPairs = sortBy (\(_,x) (_,y)-> compare x y )
+
+filename :: String -> String
+filename = reverse . takeWhile (/='/') . reverse
+
+maxElemIndices :: Ord a => [a] -> [Int]
+maxElemIndices xs = findIndices (==maximum xs) xs
+
 -- EXERCISE 4
 elem' y = foldr (\x acc -> acc || x==y) False
 
-reverse' = foldr (\x acc -> acc ++ [x]) [] 
+reverse' = foldr (\x acc -> acc ++ [x]) []
+
+nubRuns :: Eq a => [a] -> [a]
+nubRuns (x:xs) = foldr (\x acc -> acc ++ (if (last acc) == x then [] else [x])) [x] xs
 
 -- EXERCISE 5
 reverse'' :: [a] -> [a]
