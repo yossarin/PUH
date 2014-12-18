7526ec892b5d83785d9c752aa6b35ab95888d945
yossarin <yossarin.github@gmail.com>
2014-12-18 16:21:08 +0100

    HW 8 exercise 3 done.

--- lec08/homework.hs
+++ lec08/homework.hs
@@ -1,5 +1,5 @@
 import Prelude hiding (Left, Right, Up, Down)
-
+import Data.Maybe (fromJust, isJust)
 {-
  1.) small domain-specific language for enslaving turtles. 
 -}
@@ -81,3 +81,55 @@ isSubtree t1 t2@(Node _ lt rt) | consistOf t2 t1 = True
         consistOf Leaf           (Node _ _ _)            = False
         consistOf (Node x lx rx) (Node y ly ry) | x == y = consistOf lx ly && consistOf rx ry
                                                 | otherwise = False
+{-
+ 3.)
+-}
+
+data Date = Date 
+  { day   :: Int
+  , month :: Int
+  , year  :: Int
+  } deriving (Show, Eq)
+-- fuunction return True if its argument is a leap year
+isLeap :: Int -> Bool
+isLeap y = y `mod` 4 == 0 && (not (y `mod` 100 == 0) || y `mod` 400 == 0)
+-- (a) function date constructs a date from three integers. It is to be
+--     used instead of the Date constructor to check for date validity. 
+--     If the date is invalid, Nothing is returned. 
+--     Otherwise, Just Date is returned.
+date :: Int -> Int -> Int -> Maybe Date
+date d m y = case (d, m, isLeap y) of
+  (d, 2, True) -> if d `elem` [1..29] then Just (Date d 2 y) else Nothing
+  (d, 2, _   ) -> if d `elem` [1..28] then Just (Date d 2 y) else Nothing
+  (d, m, _   ) -> case m `elem` [1,3,5,7,8,10,12] of
+                    True -> if d `elem` [1..31] then Just (Date d m y) else Nothing
+                    _    -> if d `elem` [1..30] then Just (Date d m y) else Nothing
+
+-- (b) function addDays takes a Date and an Int as a number of days
+--     to add and calculates the new Date.
+positive :: Date -> Int -> Date
+positive (Date d m y) n = case isJust $ date (d+n) m y of
+      True  -> fromJust $ date (d+n) m y
+      False -> case (m, isLeap y) of
+                 ( 2, True) -> fromJust $ date (d+n-29) 3 y
+                 ( 2, _   ) -> fromJust $ date (d+n-28) 3 y
+                 (12, _   ) -> fromJust $ date (d+n-31) 1 (y+1)
+                 _         -> case m `elem` [1,3,5,7,8,10] of
+                                True -> fromJust $ date (d+n-31) (m+1) y
+                                _    -> fromJust $ date (d+n-30) (m+1) y
+
+negative :: Date -> Int -> Date
+negative (Date d m y) n = case isJust $ date (d+n) m y of
+      True  -> fromJust $ date (d+n) m y
+      False -> case (m, isLeap y) of
+                 (2, True) -> fromJust $ date (d+n+29) 1 y
+                 (2, _   ) -> fromJust $ date (d+n+28) 1 y
+                 (1, _   ) -> fromJust $ date (d+n+31) 12 (y-1)
+                 (3, True) -> fromJust $ date (d+n+29) 2 y
+                 (3, _   ) -> fromJust $ date (d+n+28) 2 y
+                 _         -> case m `elem` [3,5,7,8,10,12] of
+                                True -> fromJust $ date (d+n+31) (m-1) y
+                                _    -> fromJust $ date (d+n+30) (m-1) y
+
+addDays :: Date -> Int -> Date
+addDays dat@(Date d m y) n = if n < 0 then negative dat n else positive dat n
